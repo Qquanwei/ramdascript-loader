8b091df0c9eb618821664e179b9a215c977f5aae
quanwei <weiquan@weideMacBook-Pro.local>
2017-06-24 00:07:02 +0800

    fix compile to commonjs and require ramda

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var compiler = require('ramdascript').compile
 
-
 module.exports = function (source) {
-    this.cacheable && this.cacheable()
-    return compiler(source)
+  this.cacheable && this.cacheable()
+  return compiler(source, { wrapper: 'commonjs'})
 }
