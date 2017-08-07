815f3053cf95c63ab14e93e7f024a9b813270adf
Yosbel Marin <yosbel@yosbel.com>
2017-08-07 16:20:49 -0400

    add support for RamdaScript 0.5

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-var compiler = require('ramdascript').compile
+var ram = require('ramdascript')
 
 module.exports = function (source) {
-  this.cacheable && this.cacheable()
-  return compiler(source, { wrapper: 'commonjs'})
+	this.cacheable && this.cacheable()
+	var result = ram.compile(source, {
+		format: 'cjs',
+		sourceMap: true,
+		sourceMapCfg: {
+			sourceRoot: '',
+			source: this.remainingRequest,
+			file: this.currentRequest,
+			sourceContent: source
+		}
+	})
+	var sourceMap = JSON.parse(result.sourceMap)
+	this.callback(null, result.js, sourceMap)
 }
